5abe44c7980b7574cd4c0c7d679891eae3f1828e
Tyler Cromwell <tyler@csh.rit.edu>
2015-02-11 00:40:16 -0500

    Removed itoa test

--- kernel.c
+++ kernel.c
@@ -9,10 +9,7 @@ void kernel(void) {
     size_t slen = strlen(started);
     size_t hlen = strlen(halted);
 
-    char* s = itoa(34637472);
-
     console_clear();
-    console_write_line(s, 2, FG_GREY_L);
     console_write_line(started, slen, FG_GREEN);
 
     console_write_line(halted, hlen, FG_RED);
