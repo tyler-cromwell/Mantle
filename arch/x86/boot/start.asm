4395fbd01f81e52a3803913dfa5e89dbb65f0207
Tyler Cromwell <tyler@csh.rit.edu>
2015-03-08 12:28:53 -0400

    Probably don't need to check for CPUID

--- arch/x86/boot/start.asm
+++ arch/x86/boot/start.asm
@@ -18,55 +18,15 @@ section .data
 global start
 ; Kernel functions
 extern console_clear
-extern console_write
-extern console_write_line
 extern detect_long_mode
 extern disable_paging
 extern kernel
-
-; Checks for CPUID
-detect_cpuid:
-    pushfd
-    pop eax
-    mov ecx, eax
-    xor eax, 0x200000
-    push eax
-    popfd
-    pushfd
-    pop eax
-    push ecx
-    popfd
-    xor eax, ecx
-    jnz cpuid_found
-    jz stop
-    ret
-
-; Prints confirmation message if CPUID is found
-cpuid_found:
-    push ebp
-    mov ebp, esp
-
-    push 0x07
-    push file_len
-    push file
-    call console_write
-    add esp, 12
-
-    push 0x0e
-    push found_len
-    push found
-    call console_write_line
-    add esp, 12
-
-    pop ebp
-    ret
 
 ; Entry point into the kernel binary
 start:
     cli                     ; Ignore Maskable Interrupts
     call console_clear
 
-    call detect_cpuid
     call detect_long_mode
     call disable_paging
 
@@ -74,9 +34,6 @@ start:
     call kernel             ; Actually start the Kernel
 
     hlt; and catch fire
-
-stop:
-    hlt
 
 section .bss
     align 4
