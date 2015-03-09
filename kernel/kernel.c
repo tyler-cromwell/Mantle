fcae8f453d28aa0c2c5b4f98b2da1fb0003e79fa
Tyler Cromwell <tyler@csh.rit.edu>
2015-03-09 10:40:26 -0400

    Gave filename variable static file scope

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,12 +3,14 @@
 #include <kernel/string.h>
 #include <x86/cpuid.h>
 
+static char* file = "["_FILE_"]: ";
+static size_t file_l = 12;
+
 /*
  * The main kernel function; this is where Humboldt begins operation.
  * The system will halt when/if this function returns.
  */
 void kernel(void) {
-    char* file = "["_FILE_"]: ";
     char* started = "Kernel started";
     char* halted = "System halted";
     char* field = "Vendor_id: ";
@@ -16,15 +18,15 @@ void kernel(void) {
     char id[13] = {0};
     cpuid_vendor(id);
 
-    console_write(file, strlen(file), FG_GREY_L);
+    console_write(file, file_l, FG_GREY_L);
     console_write_line(started, strlen(started), FG_GREEN);
 
-    console_write(file, strlen(file), FG_GREY_L);
+    console_write(file, file_l, FG_GREY_L);
     console_write(field, strlen(field), FG_WHITE);
     console_write_line(id, 13, FG_WHITE);
 
-    console_write(file, strlen(file), FG_GREY_L);
+    console_write(file, file_l, FG_GREY_L);
     console_write_line(halted, strlen(halted), FG_RED);
 
-    return; /* Halt the system (the 'hlt' instruction is next) */
+    return; /* Halt the system */
 }
