31127f134dd6701fc07c4a2040fa8b1f2b5609e4
Tyler Cromwell <tyler@csh.rit.edu>
2015-03-12 18:06:11 -0400

    Disables paging and prints a failure message

--- arch/x86/boot/longmode.asm
+++ arch/x86/boot/longmode.asm
@@ -3,16 +3,19 @@ global disable_paging
 extern console_write
 
 section .data
-    file        db '[', _FILE_, ']: ', 0x0
-    failed      db 'Cannot switch into Long mode. Halting...', 0x0
-    success     db 'Long mode is supported', 0x0a, 0x0
+    file            db '[', _FILE_, ']: ', 0x0
+    failed          db 'Cannot switch into Long mode. Halting...', 0x0
+    success         db 'Long mode is supported', 0x0a, 0x0
+    disabled        db 'Paging disabled', 0x0a, 0x0
 
     file_len        equ $-file
     failed_len      equ $-failed
     success_len     equ $-success
+    disabled_len    equ $-disabled
 
 section .text
 
+; Checks to see if the CPU supports Long mode
 detect_long_mode:
     mov eax, 0x80000000
     cpuid
@@ -42,12 +45,35 @@ detect_long_mode:
     pop ebp
     ret
 
+; Disables Protected mode Paging
 disable_paging:
     mov eax, cr0
-    and eax, 0x7fffffff
-    mov cr0, eax
+    and eax, 0x7fffffff ; Unset the Paging bit
+    cmp eax, 0x7fffffff ; Ensure Paging bit is 0 (probably an unnecessary step)
+    jge cannot_switch   ; Could not disable paging
+
+    mov cr0, eax        ; Load the new value back into the Control register,
+                        ; actually unsetting the Paging bit
+    push ebp
+    mov ebp, esp
+
+    push 0x07
+    push file_len
+    push file
+    call console_write
+    add esp, 12
+
+    push 0x0e
+    push disabled_len
+    push disabled
+    call console_write
+    add esp, 12
+
+    pop ebp
     ret
     
+; Prints failure message and halts the CPU if any
+; conditions for switching into Long mode are not met
 cannot_switch:
     push ebp
     mov ebp, esp
