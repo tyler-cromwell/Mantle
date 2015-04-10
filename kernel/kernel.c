c7917711358ade9c59028dcfd75c5bf527e40b86
Tyler Cromwell <tyler@csh.rit.edu>
2015-04-10 00:08:10 -0400

    Getting the size of the kernel

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -8,28 +8,39 @@
 #define file_l 12
 #endif
 
+/* Linker Script Symbol */
+extern char kernel_size[];
+
 /*
  * The main kernel function; this is where Humboldt begins operation.
  * The system will halt when/if this function returns.
  */
 void kernel(void) {
-    gdt_init();
-
     #ifdef _FILE_
     console_write(file, file_l, FG_GREY_L);
     #endif
     console_write("Kernel started\n", 15, FG_GREEN);
 
+    char* size = itoa(((uint32_t) &kernel_size) / 1024);
+
     char id[14] = {0};
     id[12] = '\n';
     cpuid_vendor(id);
 
-    char* field = "Vendor_id: ";
     #ifdef _FILE_
     console_write(file, file_l, FG_GREY_L);
     #endif
-    console_write(field, strlen(field), FG_WHITE);
+    console_write("Kernel size: ", 13, FG_WHITE);
+    console_write(size, 20, FG_WHITE);
+    console_write("KB\n", 4, FG_WHITE);
+
+    #ifdef _FILE_
+    console_write(file, file_l, FG_GREY_L);
+    #endif
+    console_write("Vendor_id: ", 12, FG_WHITE);
     console_write(id, 14, FG_WHITE);
+
+    gdt_init();
 
     #ifdef _FILE_
     console_write(file, file_l, FG_GREY_L);
