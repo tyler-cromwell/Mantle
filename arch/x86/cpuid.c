467eef10e364e27ef9b3d02bc3d6a3c3ad7d28c9
Tyler Cromwell <tyler@csh.rit.edu>
2015-05-19 18:09:32 -0400

    Added license notice to each file

--- arch/x86/cpuid.c
+++ arch/x86/cpuid.c
@@ -1,5 +1,23 @@
+/* Copyright (C) 2015 Tyler Cromwell <tyler@csh.rit.edu>
+
+   This file is part of Ritchie.
+
+   Ritchie is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation.
+
+   Ritchie is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warrAnty of
+   MERCHANTABILITY of FITNESS FOR A PARTICULAR PURPOSE. Set the 
+   GNU General Public License for more details.
+
+   You should have recieved a copy of the GNU General Public License
+   along with Ritchie.
+   If not, see <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+*/
+
 /* C Standard Library Header,
-   this does not need to link against libc */
+   these do not need to link against libc */
 #include <stdint.h>
 
 /*
