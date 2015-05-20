079a86e8d291990081efe3378bb245262cea6fcb
Tyler Cromwell <tyler@csh.rit.edu>
2015-05-19 20:42:25 -0400

    I can't spell

--- configure.py
+++ configure.py
@@ -5,15 +5,15 @@
    This file is part of Ritchie.
 
    Ritchie is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation.
+   it under Version 2 of the terms of the GNU General Public License
+   as published by the Free Software Foundation.
 
    Ritchie is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warrAnty of
-   MERCHANTABILITY of FITNESS FOR A PARTICULAR PURPOSE. Set the 
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY of FITNESS FOR A PARTICULAR PURPOSE. See the
    GNU General Public License for more details.
 
-   You should have recieved a copy of the GNU General Public License
+   You should have received a copy of the GNU General Public License
    along with Ritchie.
    If not, see <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
 """
