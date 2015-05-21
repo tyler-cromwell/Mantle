5c47fc77713d060fce3ad35f9582cf73a3d7b7c7
Tyler Cromwell <tyler@csh.rit.edu>
2015-05-20 22:39:51 -0400

    Documentation changes

--- arch/x86/gdt.c
+++ arch/x86/gdt.c
@@ -22,54 +22,21 @@
 
 /* Kernel Headers */
 #include <drivers/console.h>
-#include <x86/x86.h>
 
-/* (Information taken from osdev.org)
- * Access Byte:
- * | 7  | 6   5 | 4 | 3  | 2  | 1  | 0  |
- * +----+-------+---+----+----+----+----+
- * | Pr | Privl | 1 | Ex | DC | RW | Ac |
- * +----+-------+---+----+----+----+----+
- *
- * Pr: Present bit, must be one for all valid selectors.
- * Privl: Privilege bits (2). Contains the ring level, 0 = Kernel, 3 = User.
- * Ex: Executable bit. 1 for code selector, 0 for data selector.
- * DC: Direction/Conforming bit.
- *     Direction for data selectors:
- *         0 the segment grows up, 1 the segment grows up.
- *     Conforming for code selectors:
- *         1 if code can be executed from <= privilege level.
- *         0 if only the level set in Privl.
- * RW: Read/Write bit.
- *     For code selectors: Readable bit. Write is never allowed.
- *     For data selectors: Writable bit. Read is always allowed.
- * Ac: Accessed bit, default 0, CPU sets to 1 when accessed.
- *
- * Flags:
- * | 7  | 6  | 5 | 4 |
- * +----+----+---+---+
- * | Gr | Sz | 0 | 0 |
- * +----+----+---+---+
- *
- * Gr: Granularity Bit.
- *     0 if limit is 1 Byte blocks (block granularity).
- *     1 if limit is in 4KB blocks (page granularity).
- * Sz: Size bit.
- *     0 if 16-bit protected mode.
- *     1 if 32-bit protected mode.
- */
+/* A GDT segment descriptor */
 struct GdtDescriptor {
-    uint16_t limit;             /* 0:15 */
-    uint16_t base_low;          /* 0:15 */
-    uint8_t base_mid;           /* 16:23 */
-    uint8_t access;             /* 47:40 */
-    uint8_t granularity;        /* Limit 16:19, Flags 55:52 */
-    uint8_t base_high;          /* 24:31 */
-} __attribute__((__packed__));  /* Packed to ensure a size of 8 bytes */
+    uint16_t limit;             /* Address length */
+    uint16_t base_low;          /* Lower portion of the base address */
+    uint8_t base_mid;           /* Middle portion of the base address */
+    uint8_t access;             /* A collection of various bits */
+    uint8_t granularity;        /* Another collection of various bits */
+    uint8_t base_high;          /* Upper portion of the base address */
+} __attribute__((__packed__));
 
+/* The GDTR register */
 struct Gdtr {
-    uint16_t limit;
-    uint32_t base;
+    uint16_t limit; /* The length of the GDT */
+    uint32_t base;  /* GDT base address */
 } __attribute__((__packed__));
 
 /* The GDT and a pointer to it */
