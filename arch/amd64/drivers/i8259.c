0bd3f6918f2137092df36b007df25475d21259f3
Tyler Cromwell <tyler@csh.rit.edu>
2015-12-19 01:19:55 -0500

    Documented the functions in i8259.c

--- arch/amd64/drivers/i8259.c
+++ arch/amd64/drivers/i8259.c
@@ -54,10 +54,16 @@ void i8259_init(void) {
     outb(SLAVE_DATA,  0xff);
 }
 
+/*
+ * Masks the given IRQ.
+ * Arugment:
+ *   uint8_t irq: The IRQ number to mask.
+ */
 void i8259_set_mask(uint8_t irq) {
     uint16_t port;
     uint8_t value;
 
+    /* Determine PIC */
     if (irq < 8) {
         port = MASTER_DATA;
     } else {
@@ -69,10 +75,16 @@ void i8259_set_mask(uint8_t irq) {
     outb(port, value);
 }
 
+/*
+ * Unmasks the given IRQ.
+ * Argument:
+ *   uint8_t irq: The IRQ number to unmask.
+ */
 void i8259_clear_mask(uint8_t irq) {
     uint16_t port;
     uint8_t value;
 
+    /* Determine PIC */
     if (irq < 8) {
         port = MASTER_DATA;
     } else {
@@ -84,11 +96,17 @@ void i8259_clear_mask(uint8_t irq) {
     outb(port, value);
 }
 
+/*
+ * Disables all IRQS (masks them).
+ */
 void i8259_mask(void) {
     outb(MASTER_DATA, 0xff);
     outb(SLAVE_DATA,  0xff);
 }
 
+/*
+ * Enables all IRQS (unmasks them).
+ */
 void i8259_unmask(void) {
     outb(MASTER_DATA, 0x00);
     outb(SLAVE_DATA,  0x00);
